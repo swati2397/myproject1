31b19e473a9394a2ff9d0eb290b3d6ea9b86cd65
swati2397 <swatikale8181@gmail.com>
2025-03-19 18:56:35 +0530

    Update api.js

--- api.js
+++ api.js
@@ -6,32 +6,32 @@ const movieCategories = [
             {
                 title: "Squid Game",
                 image: "https://ntvb.tmsimg.com/assets/p20492187_b_h8_aa.jpg?w=960&h=540",
-                link: "https://www.netflix.com/title/81040344" // Link to watch
+                link: "https://www.netflix.com/title/81040344" 
             },
             {
                 title: "Misatched",
                 image: "https://ntvb.tmsimg.com/assets/p19074592_b_h10_aa.jpg?w=1280&h=720",
-                link: "https://www.netflix.com/title/80220079" // Link to watch
+                link: "https://www.netflix.com/title/80220079" 
             },
             {
                 title: "Lucifer",
                 image: "https://i.pinimg.com/originals/a1/64/ea/a164ea446979e420d4fc6d90abbb8f62.jpg",
-                link: "https://www.netflix.com/title/80057918" // Link to watch
+                link: "https://www.netflix.com/title/80057918" 
             },
             {
                 title: "Heeramandi",
                 image: "https://tse4.mm.bing.net/th?id=OIP.isIOhMfXpMNMOP3kgzIGnQHaEK&pid=Api&P=0&h=180",
-                link: "https://www.netflix.com/title/81122198" // Link to watch
+                link: "https://www.netflix.com/title/81122198" 
             },
             {
                 title: "Alice in Borderland",
                 image: "https://viniloblog.com/wp-content/uploads/2021/04/alice-in-borderland-poster-768x474.jpg",
-                link: "https://www.netflix.com/title/80200575" // Link to watch
+                link: "https://www.netflix.com/title/80200575" 
             },
             {
                 title: "Money Heist",
                 image: "https://image.tmdb.org/t/p/original/zAIippNnm6o0gYEtjapbjQSxP8G.jpg",
-                link: "https://www.netflix.com/title/80192098" // Link to watch
+                link: "https://www.netflix.com/title/80192098" 
             }
         ]
     },
@@ -42,32 +42,32 @@ const movieCategories = [
             {
                 title: "Pushpa 2",
                 image: "https://tse1.mm.bing.net/th?id=OIP.RIfUoZFeV_LOd5hAAkta9AHaEW&pid=Api&P=0&h=180",
-                link: "https://www.netflix.com/title/82006666" // Link to watch
+                link: "https://www.netflix.com/title/82006666" 
             },
             {
                 title: "Journey 2: The Mysterious Island",
                 image: "https://www.comingsoon.net/wp-content/uploads/sites/3/2023/11/Journey-2-The-Mysterious-Island.jpg",
-                link: "https://www.netflix.com/title/70178621" // Link to watch
+                link: "https://www.netflix.com/title/70178621" 
             },
             {
                 title: "Do Patti",
                 image: "https://c.ndtvimg.com/2024-10/ti9jrgi_do-paati-movie-review_625x300_25_October_24.jpg?im=FeatureCrop,algorithm=dnn,width=1200,height=738",
-                link: "https://www.netflix.com/title/81681817" // Link to watch
+                link: "https://www.netflix.com/title/81681817" 
             },
             {
                 title: "Venom: Let There Be Carnage",
                 image: "https://sportshub.cbsistatic.com/i/2021/09/07/16b39672-7470-4389-9963-3a2a8278003d/venom-let-there-be-carnage-poster-1279326.jpg?width=1200",
-                link: "https://www.netflix.com/title/81199145" // Link to watch
+                link: "https://www.netflix.com/title/81199145" 
             },
             {
                 title: "Crew",
                 image: "https://i.ytimg.com/vi/YRGKlP6Bsjk/maxresdefault.jpg",
-                link: "https://www.netflix.com/title/81672746" // Link to watch
+                link: "https://www.netflix.com/title/81672746" 
             },
             {
                 title: "Animal",
                 image: "https://www.easterneye.biz/wp-content/uploads/2024/01/netflix-sets-jan-26-release-for-animal.webp",
-                link: "https://www.netflix.com/title/81436990" // Link to watch
+                link: "https://www.netflix.com/title/81436990" 
             }
 
         ]
@@ -79,32 +79,32 @@ const movieCategories = [
             {
                 title: "Stranger Things",
                 image: "https://www.pixelstalk.net/wp-content/uploads/images5/Cool-Stranger-Things-Desktop-Wallpaper-HD.jpg",
-                link: "https://www.netflix.com/title/80057281" // Link to watch
+                link: "https://www.netflix.com/title/80057281" 
             },
             {
                 title: "Queen Of Tears",
                 image: "https://ntvb.tmsimg.com/assets/p26780072_b_h10_af.jpg?w=1280&h=720",
-                link: "https://www.netflix.com/title/81707950" // Link to watch
+                link: "https://www.netflix.com/title/81707950" 
             },
             {
                 title: "Wednesday",
                 image: "https://www.tvinsider.com/wp-content/uploads/2022/08/wednesday-netflix.jpg",
-                link: "https://www.netflix.com/title/81231974" // Link to watch
+                link: "https://www.netflix.com/title/81231974" 
             },
             {
                 title: "vincenzo",
                 image: "https://www.themoviedb.org/t/p/original/vNWvm5JnRq3KaD7JcbVovJ8RGzX.jpg",
-                link: "https://www.netflix.com/title/81365087" // Link to watch
+                link: "https://www.netflix.com/title/81365087" 
             },
             {
                 title: "Kota Factory",
                 image: "https://ntvb.tmsimg.com/assets/p20567512_b_h8_ad.jpg?w=1280&h=720",
-                link: "https://www.netflix.com/title/81249783" // Link to watch
+                link: "https://www.netflix.com/title/81249783" 
             },
             {
                 title: "Thandel",
                 image: "https://api.educationpost.in/s3-images/1714396637225-1442736-thandel.webp",
-                link: "https://www.netflix.com/title/81784087" // Link to watch
+                link: "https://www.netflix.com/title/81784087" 
             }
         ]
     }
